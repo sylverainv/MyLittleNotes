e029a22f0669792f57f84a8292967a2accf2c3e3
sylverainv <sylverainv@gmail.com>
2021-12-27 18:25:56 -0500

    created server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
+
+  
