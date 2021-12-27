19a0d5297e22fe9dd704a12205decf70a246c098
sylverainv <sylverainv@gmail.com>
2021-12-27 18:38:29 -0500

    added html routes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const htmlRoutes = require('./routes/html')
 const app = express();
 const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"))
+app.use(htmlRoutes)
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
   });
